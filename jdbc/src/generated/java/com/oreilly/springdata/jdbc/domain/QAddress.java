ceefe14a07342677606e4cfa4aa75653aecec3c9
weiwei <weiwei02@vip.qq.com>
2017-12-03 00:51:39 +0800

    update module jdbc maven plugin querydsl to 4.1.4

--- jdbc/src/generated/java/com/oreilly/springdata/jdbc/domain/QAddress.java
+++ jdbc/src/generated/java/com/oreilly/springdata/jdbc/domain/QAddress.java
@@ -1,49 +1,74 @@
 package com.oreilly.springdata.jdbc.domain;
 
-import static com.mysema.query.types.PathMetadataFactory.*;
+import static com.querydsl.core.types.PathMetadataFactory.*;
 
-import com.mysema.query.types.path.*;
+import com.querydsl.core.types.dsl.*;
 
-import com.mysema.query.types.PathMetadata;
+import com.querydsl.core.types.PathMetadata;
 import javax.annotation.Generated;
-import com.mysema.query.types.Path;
+import com.querydsl.core.types.Path;
+
+import com.querydsl.sql.ColumnMetadata;
+import java.sql.Types;
+
+
 
 
 /**
  * QAddress is a Querydsl query type for QAddress
  */
-@Generated("com.mysema.query.sql.codegen.MetaDataSerializer")
-public class QAddress extends com.mysema.query.sql.RelationalPathBase<QAddress> {
+@Generated("com.querydsl.sql.codegen.MetaDataSerializer")
+public class QAddress extends com.querydsl.sql.RelationalPathBase<QAddress> {
 
     private static final long serialVersionUID = 207732776;
 
     public static final QAddress address = new QAddress("ADDRESS");
 
-    public final StringPath city = createString("CITY");
+    public final StringPath city = createString("city");
 
-    public final StringPath country = createString("COUNTRY");
+    public final StringPath country = createString("country");
 
-    public final NumberPath<Long> customerId = createNumber("CUSTOMER_ID", Long.class);
+    public final NumberPath<Long> customerId = createNumber("customerId", Long.class);
 
-    public final NumberPath<Long> id = createNumber("ID", Long.class);
+    public final NumberPath<Long> id = createNumber("id", Long.class);
 
-    public final StringPath street = createString("STREET");
+    public final StringPath street = createString("street");
 
-    public final com.mysema.query.sql.PrimaryKey<QAddress> sysPk10029 = createPrimaryKey(id);
+    public final com.querydsl.sql.PrimaryKey<QAddress> sysPk10088 = createPrimaryKey(id);
 
-    public final com.mysema.query.sql.ForeignKey<QCustomer> addressCustomerRef = createForeignKey(customerId, "ID");
+    public final com.querydsl.sql.ForeignKey<QCustomer> addressCustomerRef = createForeignKey(customerId, "ID");
 
     public QAddress(String variable) {
         super(QAddress.class, forVariable(variable), "PUBLIC", "ADDRESS");
+        addMetadata();
     }
 
-    @SuppressWarnings("all")
+    public QAddress(String variable, String schema, String table) {
+        super(QAddress.class, forVariable(variable), schema, table);
+        addMetadata();
+    }
+
+    public QAddress(String variable, String schema) {
+        super(QAddress.class, forVariable(variable), schema, "ADDRESS");
+        addMetadata();
+    }
+
     public QAddress(Path<? extends QAddress> path) {
-        super((Class)path.getType(), path.getMetadata(), "PUBLIC", "ADDRESS");
+        super(path.getType(), path.getMetadata(), "PUBLIC", "ADDRESS");
+        addMetadata();
     }
 
-    public QAddress(PathMetadata<?> metadata) {
+    public QAddress(PathMetadata metadata) {
         super(QAddress.class, metadata, "PUBLIC", "ADDRESS");
+        addMetadata();
+    }
+
+    public void addMetadata() {
+        addMetadata(city, ColumnMetadata.named("CITY").withIndex(4).ofType(Types.VARCHAR).withSize(255));
+        addMetadata(country, ColumnMetadata.named("COUNTRY").withIndex(5).ofType(Types.VARCHAR).withSize(255));
+        addMetadata(customerId, ColumnMetadata.named("CUSTOMER_ID").withIndex(2).ofType(Types.BIGINT).withSize(64));
+        addMetadata(id, ColumnMetadata.named("ID").withIndex(1).ofType(Types.BIGINT).withSize(64).notNull());
+        addMetadata(street, ColumnMetadata.named("STREET").withIndex(3).ofType(Types.VARCHAR).withSize(255));
     }
 
 }
--- jdbc/src/generated/java/com/oreilly/springdata/jdbc/domain/QCustomer.java
+++ jdbc/src/generated/java/com/oreilly/springdata/jdbc/domain/QCustomer.java
@@ -1,47 +1,71 @@
 package com.oreilly.springdata.jdbc.domain;
 
-import static com.mysema.query.types.PathMetadataFactory.*;
+import static com.querydsl.core.types.PathMetadataFactory.*;
 
-import com.mysema.query.types.path.*;
+import com.querydsl.core.types.dsl.*;
 
-import com.mysema.query.types.PathMetadata;
+import com.querydsl.core.types.PathMetadata;
 import javax.annotation.Generated;
-import com.mysema.query.types.Path;
+import com.querydsl.core.types.Path;
+
+import com.querydsl.sql.ColumnMetadata;
+import java.sql.Types;
+
+
 
 
 /**
  * QCustomer is a Querydsl query type for QCustomer
  */
-@Generated("com.mysema.query.sql.codegen.MetaDataSerializer")
-public class QCustomer extends com.mysema.query.sql.RelationalPathBase<QCustomer> {
+@Generated("com.querydsl.sql.codegen.MetaDataSerializer")
+public class QCustomer extends com.querydsl.sql.RelationalPathBase<QCustomer> {
 
     private static final long serialVersionUID = -325328342;
 
     public static final QCustomer customer = new QCustomer("CUSTOMER");
 
-    public final StringPath emailAddress = createString("EMAIL_ADDRESS");
+    public final StringPath emailAddress = createString("emailAddress");
 
-    public final StringPath firstName = createString("FIRST_NAME");
+    public final StringPath firstName = createString("firstName");
 
-    public final NumberPath<Long> id = createNumber("ID", Long.class);
+    public final NumberPath<Long> id = createNumber("id", Long.class);
 
-    public final StringPath lastName = createString("LAST_NAME");
+    public final StringPath lastName = createString("lastName");
 
-    public final com.mysema.query.sql.PrimaryKey<QCustomer> sysPk10026 = createPrimaryKey(id);
+    public final com.querydsl.sql.PrimaryKey<QCustomer> sysPk10086 = createPrimaryKey(id);
 
-    public final com.mysema.query.sql.ForeignKey<QAddress> _addressCustomerRef = createInvForeignKey(id, "CUSTOMER_ID");
+    public final com.querydsl.sql.ForeignKey<QAddress> _addressCustomerRef = createInvForeignKey(id, "CUSTOMER_ID");
 
     public QCustomer(String variable) {
         super(QCustomer.class, forVariable(variable), "PUBLIC", "CUSTOMER");
+        addMetadata();
     }
 
-    @SuppressWarnings("all")
+    public QCustomer(String variable, String schema, String table) {
+        super(QCustomer.class, forVariable(variable), schema, table);
+        addMetadata();
+    }
+
+    public QCustomer(String variable, String schema) {
+        super(QCustomer.class, forVariable(variable), schema, "CUSTOMER");
+        addMetadata();
+    }
+
     public QCustomer(Path<? extends QCustomer> path) {
-        super((Class)path.getType(), path.getMetadata(), "PUBLIC", "CUSTOMER");
+        super(path.getType(), path.getMetadata(), "PUBLIC", "CUSTOMER");
+        addMetadata();
     }
 
-    public QCustomer(PathMetadata<?> metadata) {
+    public QCustomer(PathMetadata metadata) {
         super(QCustomer.class, metadata, "PUBLIC", "CUSTOMER");
+        addMetadata();
+    }
+
+    public void addMetadata() {
+        addMetadata(emailAddress, ColumnMetadata.named("EMAIL_ADDRESS").withIndex(4).ofType(Types.VARCHAR).withSize(255));
+        addMetadata(firstName, ColumnMetadata.named("FIRST_NAME").withIndex(2).ofType(Types.VARCHAR).withSize(255));
+        addMetadata(id, ColumnMetadata.named("ID").withIndex(1).ofType(Types.BIGINT).withSize(64).notNull());
+        addMetadata(lastName, ColumnMetadata.named("LAST_NAME").withIndex(3).ofType(Types.VARCHAR).withSize(255));
     }
 
 }
